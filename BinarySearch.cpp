c981656b4aeaa5df4381082de30b7d005c4be3b0
Gabe <1937358754@qq.com>
2020-03-30 18:37:33 +0800

    a new commit

---
+++ BinarySearch.cpp
@@ -0,0 +1,67 @@
+/*
+二分查找某个元素，查找左边界，查找右边界。
+总结如下
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+//vector<int>nums;
+int binary_search(vector<int>nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else if(nums[mid] == target) {
+            // 直接返回
+            return mid;
+        }
+    }
+    // 直接返回
+    return -1;
+}
+
+int left_bound(vector<int>nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] == target) {
+            // 别返回，锁定左侧边界
+            right = mid - 1;
+        }
+    }
+    // 最后要检查 left 越界的情况
+    if (left >= nums.size() || nums[left] != target)
+        return -1;
+    return left;
+}
+
+
+int right_bound(vector<int>nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] == target) {
+            // 别返回，锁定右侧边界
+            left = mid + 1;
+        }
+    }
+    // 最后要检查 right 越界的情况
+    if (right < 0 || nums[right] != target)
+        return -1;
+    return right;
+}
+int main(){
+    vector<int>nums = {1,2,2,3,4,6};
+    cout<<right_bound(nums,6);
+}
